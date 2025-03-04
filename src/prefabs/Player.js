606727c95fc0f2b7c2c50cd32da087b854844e94
Jonathan Ng <m8ames@gmail.com>
2025-03-03 23:35:19 -0800

    Added character movement

---
+++ src/prefabs/Player.js
@@ -0,0 +1,108 @@
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame)
+
+        scene.add.existing(this)
+        scene.physics.add.existing(this)
+
+        this.body.setSize(this.width / 2, this.height / 2)
+        this.body.setCollideWorldBounds(true)
+        this.body.setGravityY(100)
+
+        this.isJump = false
+
+        this.jumpForce = -100
+        this.velocity = 100
+
+        scene.playerFSM = new StateMachine('idle', {
+            idle: new IdleState(),
+            move: new MoveState(),
+            jump: new JumpState(),
+            hurt: new HurtState(),
+        }, [scene, this])
+    }
+
+    
+
+
+}
+
+class JumpState extends State {
+    enter(scene, player) {
+        const { left, right, up, down, space, shift } = scene.keys
+        const HKey = scene.keys.HKey
+
+        let moveDirection = new Phaser.Math.Vector2(0, 0)
+        
+        console.log("jumps")
+        // normalize movement vector, update hero position, and play proper animation
+        moveDirection.normalize()
+        player.setVelocityY(player.jumpForce)
+
+        this.stateMachine.transition('idle')
+        return
+        //player.anims.play(`walk-${player.direction}`, true)
+    }
+}
+
+class IdleState extends State {
+    enter(scene, player) {
+        player.setVelocityX(0)
+
+    }
+
+    execute(scene, player) {
+        const { left, right, up, down, space, shift } = scene.keys
+        const HKey = scene.keys.HKey
+
+        if(left.isDown || right.isDown) {
+            this.stateMachine.transition('move')
+            return
+        }
+        if(up.isDown && !this.isJump) {
+            this.stateMachine.transition('jump')
+            return
+        }
+    }
+}
+
+class MoveState extends State {
+    execute(scene, player) {
+        const { left, right, up, down, space, shift } = scene.keys
+        const HKey = scene.keys.HKey
+
+        if(!(left.isDown || right.isDown)) {
+            this.stateMachine.transition('idle')
+            return
+        }
+        if(up.isDown && !this.isJump) {
+            this.stateMachine.transition('jump')
+            return
+        }
+
+        let moveDirection = new Phaser.Math.Vector2(0, 0)
+        
+        if(left.isDown) {
+            moveDirection.x = -1
+            player.direction = 'left'
+        } else if(right.isDown) {
+            moveDirection.x = 1
+            player.direction = 'right'
+        }
+        // normalize movement vector, update hero position, and play proper animation
+        moveDirection.normalize()
+        player.setVelocityX(player.velocity * moveDirection.x)
+        //player.anims.play(`walk-${player.direction}`, true)
+    }
+}
+
+class HurtState extends State {
+    enter(scene, player) {
+        player.setVelocity(0)
+
+        scene.time.delayedCall(100, () => {
+            player.clearTint()
+            this.stateMachine.transition('idle')
+        })
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,100 +4,25 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-
-        this.isRunning = true
-
-        this.currentLetter = 0
-        this.count = 1000
-        this.score = 0
-
-        this.wordList = ["plane", "hi", "cow", "make", "half", "bard", "dock", "apple", "carrot"]
-
-        this.wordPresent = []
-        this.wordPresentTxt = []
         
         console.log("play started")
-        this.input.keyboard.on("keydown", event => {
-            if(event.keyCode == 37) {
-                for (let i = 0; i < this.wordPresent.length; i++) {
-                    if (this.wordPresent[i].index != 0)
-                    {
-                        console.log("caught")
-                        this.currentLetter = this.wordPresent[i].current
-                        this.wordPresent[i].enabled = false
-                        console.log(this.currentLetter)
-                    }
-                }
-            }
-            else if(event.keyCode == 39) {
-                for (let i = 0; i < this.wordPresent.length; i++) {
-                    if (this.wordPresent[i].index != 0)
-                    {
-                        console.log("caught")
-                        this.currentLetter = this.wordPresent[i].current
-                        this.wordPresent[i].enabled = false
-                        console.log(this.currentLetter)
-                    }
-                }
-            }
-        })
-        this.add.rectangle(0, 0, this.game.config.width,this.game.config.width, 0x00FF00).setOrigin(0,0)
         
-        let wordConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'center',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 500
-        }
+        this.add.rectangle(0,800, this.game.config.width, this.game.config.height, 0X000000).setOrigin(0,0)
+    
+        this.cameras.main.setBackgroundColor("#FACADE")
+        
+        this.keys = this.input.keyboard.createCursorKeys()
+        this.keys.HKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H)
 
-        this.input.keyboard.on("keycombomatch",  (combo, event) => {
-            console.log('Konami Code entered!')
-            this.wordFound()            
-            for (let i = 0; i < this.wordPresent.length; i++) {
-                //console.log(this.wordPresent[i])
-                
-                if (combo.keyCodes == this.wordPresent[i].keyCodes) {
-                    this.removeWord(i)
-                }
-            }
-        });
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
-        keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
-    }
+        this.player = new Player(this, 200, 160, 'player', 0)
+
+
+
+        }
 
     update() {
-        if(this.isRunning) {
-            
-        }
+        this.playerFSM.step()
     }
 
-    textUpdate() {
-        this.wordUI.text = "Word:\n"
-        for (let i = 0; i < this.wordPresent.length; i++) {
-            this.wordUI.text += this.wordPresentTxt[i] + "\n"
-        }
-
-        this.scoreUI.text = "Score: " + this.score
-        this.timerUI.text = "time: " + this.count
-    }
-
-    checkCollision(char, rock) {
-        // simple AABB checking 
-        if( rock.x < char.x + char.width && 
-            rock.x + rock.width > char.x && 
-            rock.y < char.y + char.height && 
-            rock.height + rock.y > char.y ) {
-            return true
-        } else {
-            console.log("not hit")
-            return false
-        }
-    }
+    
 }
